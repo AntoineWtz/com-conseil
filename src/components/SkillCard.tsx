8ecc95dd31314dbe57033227e7a76320eddaf3ee
Antoine Wurtz <antoine.wurtz@crowdaa.com>
2024-06-03 23:37:30 +0200

    some css

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -9,7 +9,7 @@ interface SkillCardProps {
 const SkillCard: React.FC<SkillCardProps> = ({ title, description, imageUrl }) => {
     return (
         <div className="max-w-sm rounded-xl bg-blue-100 overflow-hidden shadow-lg m-4">
-            <img className="w-full h-48 object-cover" src={imageUrl} alt={title} />
+            <img className="w-full h-48 object-cover hover:scale-105 duration-300" src={imageUrl} alt={title} />
             <div className="px-6 py-4">
                 <div className="font-bold text-[#344697] text-xl mb-2">{title}</div>
                 <p className="text-gray-700">{description}</p>
