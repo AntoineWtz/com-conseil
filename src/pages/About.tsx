176f6d5f47f48251ba81d6552d07f8aea92be35c
Antoine Wurtz <antoine.wurtz@crowdaa.com>
2024-06-05 11:51:03 +0200

    responsive about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,40 +6,40 @@ import logo3 from '../assets/images/com_conseil_logo3.jpg';
 const About: React.FC = () => {
     return (
         <MainLayout>
-            <section className="mt-8 px-4">
-                <h2 className="text-4xl text-[#344697] font-bold mb-4 text-center">Notre ADN</h2>
+            <section className="mt-12 px-4">
+                <h2 className="text-4xl text-[#344697] font-bold text-center">Notre ADN</h2>
                 <div className='flex flex-col lg:flex-row justify-evenly items-center'>
-                    <p className='mt-12 text-lg lg:w-1/2 text-center lg:text-left'>
+                    <p className='mt-12 text-justify lg:text-lg lg:w-1/2  lg:text-left'>
                         « Notre souhait est de créer le cabinet de demain et d’apporter une qualité de services et une réactivité accrues à vos besoins.<br /><br />
                         En accord avec nos valeurs Proximité, Partage et Esprit d’équipe.<br /><br />
                         Pour cela notre équipe est dotée de toutes les nouvelles technologies et logiciels mis à la disposition de notre profession afin de dématérialiser l’intégralité de vos dossiers. Cette organisation nous permet d’être au plus près de la gestion au quotidien de votre entreprise.<br /><br />
                         Pour C&C Com’Conseil, vous êtes un client unique que l’on souhaite accompagner et conseiller dans les moments importants de votre entreprise et/ou de votre vie en prenant le temps de vous écouter. »
                     </p>
-                    <img src={logo2} alt="logo2" className="h-64 m-12" />
+                    <img src={logo2} alt="logo2" className="lg:h-64 lg:m-12 h-0" />
                 </div>
-                <h3 className="text-3xl text-[#344697] font-bold m-12 text-center">Les avantages C&C Com' Conseil : la mise à disposition d'une plateforme collaborative aux clients</h3>
+                <h3 className="text-2xl lg:text-3xl text-[#344697] font-bold mt-12 mb-12 lg:m-20 text-center">Mise à disposition d'une plateforme collaborative</h3>
                 <div className="relative pt-[40%] h-0">
                     <iframe
-                        className="absolute top-0 left-0 w-full h-full rounded-xl"
+                        className="absolute top-0 left-0 w-full lg:h-full aspect-video rounded-xl"
                         src="https://www.youtube.com/embed/TX1CGuaR-MY?si=U8BnAvmanA8uks3m"
                         title="C&C Com' Conseil - Plateforme Collaborative"
                         allow="encrypted-media; web-share">
                     </iframe>
                 </div>
-                <div>
-                    <p className="text-3xl text-[#344697] font-bold m-12 text-center">L'équipe C&C Com' Conseil</p>
-                    <div className="flex flex-col md:flex-row justify-evenly items-center m-8">
-                        <div className='flex flex-col items-center bg-blue-100 text-center text-blue-800 p-6 rounded-lg shadow-md mb-8 md:mb-0'>
+                <div className='mt-24'>
+                    <p className="text-2xl lg:text-3xl text-[#344697] font-bold lg:m-20 text-center mb-12">L'équipe C&C Com' Conseil</p>
+                    <div className="flex flex-col md:flex-row justify-evenly items-center">
+                        <div className='flex flex-col items-center bg-blue-100 text-center text-blue-800 p-6 rounded-lg shadow-md mb-12'>
                             <img src={logo3} alt="logo" className="w-60 h-60 rounded" />
                             <h3 className='font-bold m-2'>Claudia PATRICIO PIRES</h3>
                             <p>Expert-comptable</p>
                         </div>
-                        <div className='flex flex-col items-center bg-blue-100 text-center text-blue-800 p-6 rounded-lg shadow-md mb-8 md:mb-0'>
+                        <div className='flex flex-col items-center bg-blue-100 text-center text-blue-800 p-6 rounded-lg shadow-md mb-12'>
                             <img src={logo2} alt="logo" className="w-60 h-60 rounded" />
                             <h3 className='font-bold m-2'>Cyril Herbillon</h3>
                             <p>Directeur de Mission</p>
                         </div>
-                        <div className='flex flex-col items-center bg-blue-100 text-center text-blue-800 p-6 rounded-lg shadow-md'>
+                        <div className='flex flex-col items-center bg-blue-100 text-center text-blue-800 p-6 rounded-lg shadow-md mb-12'>
                             <img src={logo3} alt="logo" className="w-60 h-60 rounded" />
                             <h3 className='font-bold m-2'>Romain JOUAULT</h3>
                             <p>Assistant Comptable</p>
