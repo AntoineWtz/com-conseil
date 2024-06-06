f7db84ba21bea6969f9178ed6f3b7b0fed706d20
Antoine Wurtz <antoine.wurtz@crowdaa.com>
2024-06-06 16:48:42 +0200

    some hover effect

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -16,8 +16,8 @@ const SkillCard: React.FC<SkillCardProps> = ({ id, title, description, imageUrl 
     };
 
     return (
-        <div className="max-w-52 rounded-xl bg-blue-100 overflow-hidden shadow-lg m-4 cursor-pointer" onClick={handleCardClick}>
-            <img className="w-full h-40 object-cover hover:scale-105 duration-300" src={imageUrl} alt={title} />
+        <div className="max-w-52 rounded-xl bg-blue-100 overflow-hidden shadow-lg m-4 cursor-pointer hover:scale-95 hover:shadow-xl duration-300" onClick={handleCardClick}>
+            <img className="w-full h-40 object-cover" src={imageUrl} alt={title} />
             <div className="px-4 py-4">
                 <div className="font-bold text-[#344697] text-xl text-center">{title}</div>
             </div>
