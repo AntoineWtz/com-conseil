788d617a02c3a6a2d0fa9bd6858c363e9ba30d9b
Antoine Wurtz <antoine.wurtz@crowdaa.com>
2024-06-06 17:08:15 +0200

    hover/active effect again

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -6,50 +6,50 @@ const ContactForm: React.FC = () => {
             <p className="mb-4">Pour toute question, veuillez remplir ce formulaire. Nous vous apporterons une réponse rapide.</p>
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                    <label className="block uppercase tracking-wide text-[#208DB2] text-xs font-bold mb-2" htmlFor="grid-first-name">
                         Prénom
                     </label>
-                    <input className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Jane" />
+                    <input className="appearance-none block w-full bg-gray-100 text-[#208DB2] border border-gray-200 rounded-lg py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-[#344697]" id="grid-first-name" type="text" placeholder="Jane" />
                 </div>
                 <div className="w-full md:w-1/2 px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                    <label className="block uppercase tracking-wide text-[#208DB2] text-xs font-bold mb-2" htmlFor="grid-last-name">
                         Nom
                     </label>
-                    <input className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+                    <input className="appearance-none block w-full bg-gray-100 text-[#208DB2] border border-gray-200 rounded-lg py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-[#344697]" id="grid-last-name" type="text" placeholder="Doe" />
                 </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-email">
+                    <label className="block uppercase tracking-wide text-[#208DB2] text-xs font-bold mb-2" htmlFor="grid-email">
                         Email
                     </label>
-                    <input className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="email@entreprise.com" />
+                    <input className="appearance-none block w-full bg-gray-100 text-[#208DB2] border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[#344697]" id="grid-email" type="email" placeholder="email@entreprise.com" />
                 </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-phone">
+                    <label className="block uppercase tracking-wide text-[#208DB2] text-xs font-bold mb-2" htmlFor="grid-phone">
                         Téléphone
                     </label>
-                    <input className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-phone" type="phone" placeholder="0102030405" />
+                    <input className="appearance-none block w-full bg-gray-100 text-[#208DB2] border border-gray-200 rounded-lg py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-[#344697]" id="grid-phone" type="phone" placeholder="0102030405" />
                 </div>
                 <div className="w-full md:w-1/2 px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-company">
+                    <label className="block uppercase tracking-wide text-[#208DB2] text-xs font-bold mb-2" htmlFor="grid-company">
                         Société
                     </label>
-                    <input className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-company" type="text" placeholder="Com'Conseil" />
+                    <input className="appearance-none block w-full bg-gray-100 text-[#208DB2] border border-gray-200 rounded-lg py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-[#344697]" id="grid-company" type="text" placeholder="Com'Conseil" />
                 </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-message">
+                    <label className="block uppercase tracking-wide text-[#208DB2] text-xs font-bold mb-2" htmlFor="grid-message">
                         Message
                     </label>
-                    <textarea className="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-message" rows={8}></textarea>
+                    <textarea className="appearance-none block w-full bg-gray-100 text-[#208DB2] border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[#344697]" id="grid-message" rows={8}></textarea>
                 </div>
             </div>
             <div className="flex justify-center">
-                <button className="bg-[#208DB2] hover:bg-[#344697] duration-200 text-white font-bold py-2 px-4 rounded" type="button">
+                <button className="bg-[#208DB2] hover:bg-[#344697] duration-200 text-white font-bold py-2 px-4 rounded-lg" type="button">
                     Envoyer
                 </button>
             </div>
